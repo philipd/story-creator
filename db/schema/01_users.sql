159ebfcf8cf47be339145662117812b56f31abf3
David Ramalho <david.m.ramalho@gmail.com>
2020-09-19 16:47:39 +0000

    add create tables for users/stories

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,6 +1,5 @@
 -- Drop and recreate Users table (Example)
-
-DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL
