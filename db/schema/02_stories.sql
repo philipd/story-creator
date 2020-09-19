f77ee269a903dfa15c37d696eca3f99df6168787
Philip Dickau <philip.hd@protonmail.com>
2020-09-19 11:26:10 -0600

    Declared enum type outside table creation statement

--- db/schema/02_stories.sql
+++ db/schema/02_stories.sql
@@ -1,7 +1,8 @@
 -- Drop and recreate stories table (Example)
 DROP TABLE IF EXISTS stories;
+CREATE TYPE story_status AS ENUM ('complete', 'open', 'pending');
 CREATE TABLE stories (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
-  status ENUM ('complete', 'open', 'pending')
+  status story_status DEFAULT 'open'
 );
