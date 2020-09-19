3365719b49afc13afefaf382f381829a8327b0ff
Philip Dickau <philip.hd@protonmail.com>
2020-09-19 11:38:11 -0600

    Fix bugs with unnecessary commas, etc

--- db/schema/03_contributions.sql
+++ db/schema/03_contributions.sql
@@ -3,7 +3,7 @@ CREATE TABLE contributions (
   id SERIAL PRIMARY KEY NOT NULL,
   story_id INTEGER REFERENCES stories.id,
   author_id INTEGER REFERENCES users.id,
-  sequenceNumber serial,
+  sequence_number serial,
   accepted boolean DEFAULT false,
-  text text,
+  text text
 )
--- db/seeds/03_contributions.sql
+++ db/seeds/03_contributions.sql
@@ -1,4 +1,6 @@
 INSERT INTO contributions
-  (author_id, story_id, title)
+  (author_id, story_id, sequence_number, text)
 VALUES
-  (1, 1, 'The Yellow Wallpaper')
+  (1, 1, 0, 'It was a dark and stormy night'),
+  (2, 1, 1, 'My car had broken down again.'),
+  (2, 2, 0, 'Call me Ishmael')
