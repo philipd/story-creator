dc87c8d2ae87da0a68df793303e9a8fecd5ff17e
Philip Dickau <philip.hd@protonmail.com>
2020-09-19 13:51:33 -0600

    Create basic stories view

---
+++ public/scripts/stories.js
@@ -0,0 +1,31 @@
+const sampleStories = [
+  { userId: 1, title: 'Midnight\'s Children' },
+  { userId: 1, title: 'The Crying of Lot 49' },
+  { userId: 2, title: 'Oryx and Crake' }
+];
+
+const createStoryElement = function(storyData) {
+  let $story = $('<article>').addClass('story');
+  let $title = $('<div>').addClass('title').text(storyData.title);
+  $story.append($title);
+  return $story;
+};
+
+const renderStories = function(stories) {
+  for (const story of stories) {
+    let $story = createStoryElement(story);
+    $('#stories').prepend($story);
+  }
+};
+
+const loadStories = function() {
+  $.ajax('api/stories', { method: 'GET' })
+    .then(function(response) {
+      renderStories(response);
+    });
+};
+
+$(document).ready(() => {
+  // loadStories();
+  renderStories(sampleStories);
+});
