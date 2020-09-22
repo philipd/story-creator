ac9a9622e62fe5387db248c29c22d5e5bc6fbfcc
Philip Dickau <philip.hd@protonmail.com>
2020-09-22 16:07:32 -0600

    Upsert upvote

--- db/upvote-queries.js
+++ db/upvote-queries.js
@@ -1,12 +1,8 @@
 const db = require('./db');
 const vote = (user_id, contribution_id) => {
   return db.query(`
-    CASE
-      WHEN EXISTS (SELECT * FROM upvotes WHERE user_id = $1 AND contribution_id = $2)
-        THEN INSERT INTO upvotes (user_id, contribution_id) VALUES ($1, $2);
-      ELSE
-        UPDATE upvotes SET active = NOT active WHERE user_id = $1 AND contribution_id = $2;
-    END
+    INSERT INTO upvotes (user_id, contribution_id) VALUES($1, $2)
+      ON CONFLICT (user_id, contribution_id) DO UPDATE SET active = NOT upvotes.active;
   `, [user_id, contribution_id])
     .then((response) => {
       console.log(response.rows);
@@ -17,3 +13,12 @@ const vote = (user_id, contribution_id) => {
 module.exports = {
   vote
 };
+
+// `
+//     CASE
+//       WHEN EXISTS (SELECT * FROM upvotes WHERE user_id = $1 AND contribution_id = $2)
+//         THEN INSERT INTO upvotes (user_id, contribution_id) VALUES ($1, $2)
+//       ELSE
+//         UPDATE upvotes SET active = NOT active WHERE user_id = $1 AND contribution_id = $2;
+//     END
+//   `
