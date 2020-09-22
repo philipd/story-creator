044ed8439f0560a2f22adcd0cfd7887d53609d86
Philip Dickau <philip.hd@protonmail.com>
2020-09-21 20:22:36 -0600

    Fix bug where stories don't display on story page if they have no contributions

--- db/story-queries.js
+++ db/story-queries.js
@@ -18,7 +18,7 @@ const getStoriesById = (storyid) => {
   return db.query(`SELECT stories.*, contributions.*, users.*
   FROM stories
   JOIN users ON stories.user_id = users.id
-  JOIN contributions ON contributions.story_id = stories.id
+  LEFT OUTER JOIN contributions ON contributions.story_id = stories.id
   WHERE stories.id = $1`, [storyid])
     .then((response) => {
       return response.rows[0];
