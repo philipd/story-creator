bb635d0158f3af76f10d8dcab6efbef15407df98
David Ramalho <david.m.ramalho@gmail.com>
2020-09-24 18:21:01 +0000

    changed schema to fix delete

--- sql/schema/04_upvotes.sql
+++ sql/schema/04_upvotes.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS upvotes CASCADE;
 CREATE TABLE upvotes (
   user_id INTEGER REFERENCES users(id),
-  contribution_id INTEGER REFERENCES contributions(id),
+  contribution_id INTEGER REFERENCES contributions(id) ON DELETE CASCADE,
   active BOOLEAN DEFAULT true,
   PRIMARY KEY(user_id, contribution_id)
 )
