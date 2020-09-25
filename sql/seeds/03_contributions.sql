ff2d477c055f92866ab6e9a9d6ff9cf5711e733f
David Ramalho <david.m.ramalho@gmail.com>
2020-09-25 03:55:11 +0000

    added upvotes data

--- sql/seeds/03_contributions.sql
+++ sql/seeds/03_contributions.sql
@@ -7,3 +7,27 @@ insert into contributions (story_id, user_id, chapter_number, ctext, accepted) v
 insert into contributions (story_id, user_id, chapter_number, ctext, accepted) values (2, 1, 1, ' Master Luke, his rebel friends and myself had gone to Endor, the home of the Ewoks.', true);
 insert into contributions (story_id, user_id, chapter_number, ctext, accepted) values (2, 4, 2, 'The little furry Ewoks would scare anyone who dare gaze upon them. I, C-3PO, was not scared! And given that I was the only one in our party who could communicate with the furry fellas...', true);
 insert into contributions (story_id, user_id, chapter_number, ctext, accepted) values (2, 6, 3, 'I was brought to one of their huts, where proceeded to tell them about the journey we had been on. I told them about the wicked Darth Vader and our struggle against the Empire. Once I finished explaining ...', false);
+insert into contributions (story_id, user_id, chapter_number, ctext, accepted)
+values
+(12, 4, 1, 'So, Fry moved in with Bender. Bender lived in a robot apartment, which was little more than a two-cubic meter stall, and it soon became clear that Bender''s cramped apartment coult not meet Fry''s needs', true)
+,(12, 8, 2, 'The two began a search for a living space that would satisfy them both, only to conclude that none of the properties they viewed were remotely livable. But then they overhear that one of the Professor''s colleagues has died, and are able to lease his spacious, fully-furnished apartment.', true)
+,(12, 5, 3, 'The two held a housewarming party, and the guests arrived with various gifts. Unfortunately, they discovered that Bender''s antenna interfered with the building''s satellite reception, and the landlady promptly evicts them.', true)
+,(12, 1, 4, 'Ashamed at their utter failure, the two moved to a work camp on Mars, where they spent their time harvesting radioactive rocks.', false)
+,(12, 2, 4, 'Unwilling to take such an insult sitting down, the two mounted a campaign of terror against the landlady, sneaking owls into her basement, lighting fires in her garbage bins, and stealing her electricity.', false)
+,(12, 5, 4, 'The two continued their house hunt, eventually settling down in a cabin built from broken starships.', false)
+;
+
+insert into contributions (story_id, user_id, chapter_number, ctext, accepted)
+values
+(8, 2, 1, 'Such doomed romance had been with a younger Professor Hubert Farnsworth, then an employee of Mom''s Friendly Robot Company. When Mom insisted that the Professor''s latest design, a children''s toy named Q.T. McWhiskers, be changed to an eight-foot-tall death machine to be sold on the intergalactic arms market, the Professor, enraged, stormed out of the room, and they had not seen each other since.', true)
+,(8, 3, 2, 'In revenge, Mom attempts to become the ''supreme overlord of Earth'' for this Mother''s Day, ordering the entire robot population of the planet to rebel and overthrow humanity through a control that transmits to every robot''s antenna.', true)
+,(8, 5, 3, 'You stink, lol!', false)
+,(8, 4, 3, 'Bender smells hehe', false)
+;
+
+insert into contributions (story_id, user_id, chapter_number, ctext, accepted)
+values
+(15, 6, 1, 'Unfortunately, due to a programming error, the jolly robotic saint soon turned into a mad murderer when his standards were set too high. Thus, he will kill anyone who has been naughty, which, by his standards is essentially everybody.', true)
+,(15, 7, 2, 'Meanwhile, Bender volunteers at a homeless robot shelter, albeit stealing some food from there. He befriends several homeless robots and goes on a robbery spree with them.', false)
+,(15, 3, 2, 'Back at Planet Express, the Professor removes Bender''s empathy chip when Bender demands that he do so. When the empathy chip is taken out the Professor says in amazement that the chip was actually turned off.', false)
+;
