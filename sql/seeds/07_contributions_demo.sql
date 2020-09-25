38d0eb4e469866f6551c93959a236a5fcc482083
Philip Dickau <philip.hd@protonmail.com>
2020-09-24 20:19:04 -0600

    Create some demo seeds

---
+++ sql/seeds/07_contributions_demo.sql
@@ -0,0 +1,23 @@
+insert into contributions (story_id, user_id, chapter_number, ctext, accepted)
+values
+(191, 4, 1, "So, Fry moved in with Bender. Bender lived in a robot apartment, which was little more than a two-cubic meter stall, and it soon became clear that Bender's cramped apartment coult not meet Fry's needs", true)
+,(191, 1, 2, "The two began a search for a living space that would satisfy them both, only to conclude that none of the properties they viewed were remotely livable. But then they overhear that one of the Professor's colleagues has died, and are able to lease his spacious, fully-furnished apartment.", true)
+,(191, 4, 3, "The two held a housewarming party, and the guests arrived with various gifts. Unfortunately, they discovered that Bender's antenna interfered with the building's satellite reception, and the landlady promptly evicts them.", true)
+,(191, 3, 4, "Ashamed at their utter failure, the two moved to a work camp on Mars, where they spent their time harvesting radioactive rocks.", false)
+,(191, 2, 4, "Unwilling to take such an insult sitting down, the two mounted a campaign of terror against the landlady, sneaking owls into her basement, lighting fires in her garbage bins, and stealing her electricity.", false)
+,(191, 5, 4, "The two continued their house hunt, eventually settling down in a cabin built from broken starships.", false)
+;
+
+insert into contributions (story_id, user_id, chapter_number, ctext, accepted)
+values
+(192, 2, 1, "Such doomed romance had been with a younger Professor Hubert Farnsworth, then an employee of Mom's Friendly Robot Company. When Mom insisted that the Professor's latest design, a children's toy named Q.T. McWhiskers, be changed to an eight-foot-tall death machine to be sold on the intergalactic arms market, the Professor, enraged, stormed out of the room, and they had not seen each other since.", true)
+,(192, 3, 2, "In revenge, Mom attempts to become the 'supreme overlord of Earth' for this Mother's Day, ordering the entire robot population of the planet to rebel and overthrow humanity through a control that transmits to every robot's antenna.", true)
+;
+
+insert into contributions (story_id, user_id, chapter_number, ctext, accepted)
+values
+(193, 6, 1, "Unfortunately, due to a programming error, the jolly robotic saint soon turned into a mad murderer when his standards were set too high. Thus, he will kill anyone who has been naughty, which, by his standards is essentially everybody.", true)
+,(193, 7, 2, "Meanwhile, Bender volunteers at a homeless robot shelter, albeit stealing some food from there. He befriends several homeless robots and goes on a robbery spree with them.", true)
+,(193, 3, 2, "Back at Planet Express, the Professor removes Bender's empathy chip when Bender demands that he do so. When the empathy chip is taken out the Professor says in amazement that the chip was actually turned off.", true)
+,(193, 9, 2, "After training with Leela, Bender has his first fight against a chainsaw-equipped robot called The Clearcutter.", true)
+;
